20a48e2e14385a27ab87e277437e216eaf978137
KelseyJ22 <kelsey2@stanford.edu>
2017-12-31 21:11:36 -0800

    complete draft for capsule nextwork -- needs debugging

--- capsuleNetwork.py
+++ capsuleNetwork.py
@@ -1,12 +1,13 @@
 import numpy as np
 import tensorflow as tf
 
-num_capsules = 4 # TODO
 output_vec_len = 10 # TODO
-kernel_size = 10 # TODO
-stride = 1 # TODO
-num_iterations = 4 # TODO
-batch_size = 1000 # TODO
+num_iterations = 3
+batch_size = 128 
+lambda_val = 0.5
+m_plus = 0.9
+m_minus = 0.1
+lr = 1e-4
 
 def capsule(input_data, b_ij, ind_j):
 	with tf.variable_scope('routing'): # TODO: example code has this variable_scope -- do I actually need it?
@@ -46,13 +47,13 @@ def squash(input_vec):
     return(squashed)
 
 
-def build(routing, batch_size, input_data):
+def build_capsules(routing, input_data, num_units, num_capsules, kernel_size=None, stride=None):
 	if routing: # digit layer
 		# reshape to [batch_size, 1152, 8, 1]
-	    data = tf.reshape(input_data, shape=(batch_size, 1152, 8, 1)) # TODO: why is this reshaped variable never used in the example code?
+	    data = tf.reshape(input_data, shape = (batch_size, 1152, 8, 1)) # TODO: why is this reshaped variable never used in the example code?
 
 	    # b_ij: [1, num_caps_l, num_caps_l_plus_1, 1]
-	    b_ij = tf.zeros(shape=[1, 1152, 10, 1], dtype=np.float32)
+	    b_ij = tf.zeros(shape = [1, 1152, 10, 1], dtype = np.float32)
 	    capsules = []
 	    for j in range(num_capsules):
 	        with tf.variable_scope('caps_' + str(j)):
@@ -60,7 +61,7 @@ def build(routing, batch_size, input_data):
 	            capsules.append(curr)
 
 	    # form tensor of shape [batch_size, 10, 16, 1]
-	    all_capsules = tf.concat(capsules, axis=1)
+	    all_capsules = tf.concat(capsules, axis = 1)
 
 	else: # primary layer
 	    # [batch_size, 20, 20, 256]
@@ -69,68 +70,38 @@ def build(routing, batch_size, input_data):
 	        # each capsule i: [batch_size, 6, 6, 32]
 	        with tf.variable_scope('unit_' + str(i)):
 	            curr_capsule = tf.contrib.layers.conv2d(input_data, num_capsules, kernel_size, stride, padding="VALID")
-	            curr_capsule = tf.reshape(curr_capsule, shape=(batch_size, -1, 1, 1))
+	            curr_capsule = tf.reshape(curr_capsule, shape = (batch_size, -1, 1, 1))
 	            capsules.append(curr_capsule)
 
 	    # [batch_size, 1152, 8, 1]
-	    all_capsules = tf.concat(capsules, axis=2)
+	    all_capsules = tf.concat(capsules, axis = 2)
 	    all_capsules = squash(all_capsules)	    
 
 	return(all_capsules)
 
 
-def loss(X, Y, pred):
-	# margin loss
-    max_l = tf.square(tf.maximum(0., cfg.m_plus - self.v_length)) # max(0, m_plus-||v_c||)^2
-    max_r = tf.square(tf.maximum(0., self.v_length - cfg.m_minus)) # max(0, ||v_c||-m_minus)^2
+def capsule_network():
+	with tf.variable_scope('convolution_1'): # [batch_size, 20, 20, 256]
+	    conv1 = tf.contrib.layers.conv2d(X, num_outputs = 256, kernel_size = 9, stride = 1, padding='VALID')
 
-    max_l = tf.reshape(max_l, shape=(cfg.batch_size, -1))
-    max_r = tf.reshape(max_r, shape=(cfg.batch_size, -1))
+	with tf.variable_scope('primary_capsule'): # [batch_size, 1152, 8, 1]
+	    caps1 = build_capsules(routing=False, conv1, num_units = 8, num_capsules = 32, kernel_size=9, stride=2)
 
-    T_c = Y # TODO: check this
-    L_c = T_c * max_l + cfg.lambda_val * (1 - T_c) * max_r
-    margin_loss = tf.reduce_mean(tf.reduce_sum(L_c, axis=1))
-
-    # reconstruction loss
-    correct = tf.reshape(X, shape=(batch_size, -1))
-    squared = tf.square(pred - correct)
-    reconstruction_loss = tf.reduce_mean(squared)
-
-    # total loss
-    total_loss = margin_loss + 0.0005 * reconstruction_err
-
-    # summarize
-    tf.summary.scalar('margin_loss', margin_loss)
-    tf.summary.scalar('reconstruction_loss', reconstruction_err)
-    tf.summary.scalar('total_loss', total_loss)
-    tf.summary.image('reconstruction_img', tf.reshape(pred, shape=(batch_size, 28, 28, 1)))
-    merged_sum = tf.summary.merge_all()
-
-    return total_loss
-
-
-def network():
-	with tf.variable_scope('convolution_1'): # Conv1, [batch_size, 20, 20, 256]
-	    conv1 = tf.contrib.layers.conv2d(X, num_outputs=256, kernel_size=9, stride=1, padding='VALID')
-
-	with tf.variable_scope('primary_capsule'): # Primary Capsules, [batch_size, 1152, 8, 1]
-	    primaryCaps = CapsConv(num_units=8, with_routing=False)
-	    caps1 = primaryCaps(conv1, num_outputs=32, kernel_size=9, stride=2)
-
-	with tf.variable_scope('digit_capsule'): # DigitCaps layer, [batch_size, 10, 16, 1]
-	    digitCaps = CapsConv(num_units=16, with_routing=True)
-	    caps2 = digitCaps(caps1, num_outputs=10)
+	with tf.variable_scope('digit_capsule'): # [batch_size, 10, 16, 1]
+	    caps2 = build_capsules(routing=True, caps1, num_units=16, num_capsules=10)
 
 	with tf.variable_scope('masking'):
-	    v_length = tf.sqrt(tf.reduce_sum(tf.square(self.caps2), axis=2, keep_dims=True)) # calculate ||v_c||
-	    softmax_v = tf.nn.softmax(v_length, dim=1)
+		# TODO: make v_c accessible below
+	    v_c = tf.sqrt(tf.reduce_sum(tf.square(caps2), axis=2, keep_dims=True)) # calculate ||v_c||
+	    softmax_v = tf.nn.softmax(v_c, dim=1)
 
-	    argmax_ind = tf.argmax(softmax_v, axis=1, output_type=tf.int32)
+	    largest_ind = tf.argmax(softmax_v, axis=1, output_type=tf.int32)
 
 	    masked_v = []
-	    argmax_ind = tf.reshape(argmax_ind, shape=(batch_size, ))
+	    largest_ind = tf.reshape(largest_ind, shape=(batch_size, ))
+	    
 	    for batch in range(0, batch_size): # TODO: example code looks buggy here with "for batchsize in range(batchsize)"...
-	        v = caps2[batch][argmax_ind[batch], :]
+	        v = caps2[batch][largest_ind[batch], :]
 	        masked_v.append(tf.reshape(v, shape=(1, 1, 16, 1)))
 
 	    masked_v = tf.concat(masked_v, axis=0)
@@ -142,6 +113,61 @@ def network():
 	    pred = tf.contrib.layers.fully_connected(fc2, num_outputs=784, activation_fn=tf.sigmoid)
 
 
+def run_model():
+  x = tf.placeholder(tf.float32, shape = (batch_size, 28, 28, 1))
+  y_ = tf.placeholder(tf.float32, shape = (batch_size, 28, 28, 1))
+  y_pred, keep_prob = capsule_network(x)
+
+  with tf.name_scope('loss'):
+    # margin loss
+    max_l = tf.square(tf.maximum(0., m_plus - v_c)) # max(0, m_plus - ||v_c||)^2
+    max_r = tf.square(tf.maximum(0., v_c - m_minus)) # max(0, ||v_c|| - m_minus)^2
+
+    max_l = tf.reshape(max_l, shape = (batch_size, -1))
+    max_r = tf.reshape(max_r, shape = (batch_size, -1))
+
+    T_c = Y # TODO: check this
+    L_c = T_c * max_l + lambda_val * (1 - T_c) * max_r
+    margin_loss = tf.reduce_mean(tf.reduce_sum(L_c, axis=1))
+
+    # reconstruction loss
+    correct = tf.reshape(X, shape=(batch_size, -1))
+    squared = tf.square(pred - correct)
+    reconstruction_loss = tf.reduce_mean(squared)
+
+    # total loss
+    total_loss = margin_loss + 0.0005 * reconstruction_loss
+
+    # summarize
+    tf.summary.scalar('margin_loss', margin_loss)
+    tf.summary.scalar('reconstruction_loss', reconstruction_loss)
+    tf.summary.scalar('total_loss', total_loss)
+    tf.summary.image('reconstruction_img', tf.reshape(pred, shape=(batch_size, 28, 28, 1)))
+    merged_sum = tf.summary.merge_all()
+
+  with tf.name_scope('adam_optimizer'):
+    train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
+
+  with tf.name_scope('accuracy'):
+    correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y_, 1))
+    correct_prediction = tf.cast(correct_prediction, tf.float32)
+  accuracy = tf.reduce_mean(correct_prediction)
+
+  graph_location = tempfile.mkdtemp()
+  print('Saving graph to: %s' % graph_location)
+  train_writer = tf.summary.FileWriter(graph_location)
+  train_writer.add_graph(tf.get_default_graph())
+
+  with tf.Session() as sess:
+	sess.run(tf.global_variables_initializer())
+	for i in range(20000):
+		batch = mnist.train.next_batch(50)
+		if i % 100 == 0:
+			train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1]})
+			print('step %d, training accuracy %g' % (i, train_accuracy))
+		train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+	print('test accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
 
-
+run_model()
