5693db656d3b3553b28a9292db2b1116a497e437
KelseyJ22 <kelsey2@stanford.edu>
2017-12-31 16:54:02 -0800

    more on the draft -- building network, defining loss

--- capsuleNetwork.py
+++ capsuleNetwork.py
@@ -77,3 +77,71 @@ def build(routing, batch_size, input_data):
 	    all_capsules = squash(all_capsules)	    
 
 	return(all_capsules)
+
+
+def loss(X, Y, pred):
+	# margin loss
+    max_l = tf.square(tf.maximum(0., cfg.m_plus - self.v_length)) # max(0, m_plus-||v_c||)^2
+    max_r = tf.square(tf.maximum(0., self.v_length - cfg.m_minus)) # max(0, ||v_c||-m_minus)^2
+
+    max_l = tf.reshape(max_l, shape=(cfg.batch_size, -1))
+    max_r = tf.reshape(max_r, shape=(cfg.batch_size, -1))
+
+    T_c = Y # TODO: check this
+    L_c = T_c * max_l + cfg.lambda_val * (1 - T_c) * max_r
+    margin_loss = tf.reduce_mean(tf.reduce_sum(L_c, axis=1))
+
+    # reconstruction loss
+    correct = tf.reshape(X, shape=(batch_size, -1))
+    squared = tf.square(pred - correct)
+    reconstruction_loss = tf.reduce_mean(squared)
+
+    # total loss
+    total_loss = margin_loss + 0.0005 * reconstruction_err
+
+    # summarize
+    tf.summary.scalar('margin_loss', margin_loss)
+    tf.summary.scalar('reconstruction_loss', reconstruction_err)
+    tf.summary.scalar('total_loss', total_loss)
+    tf.summary.image('reconstruction_img', tf.reshape(pred, shape=(batch_size, 28, 28, 1)))
+    merged_sum = tf.summary.merge_all()
+
+    return total_loss
+
+
+def network():
+	with tf.variable_scope('convolution_1'): # Conv1, [batch_size, 20, 20, 256]
+	    conv1 = tf.contrib.layers.conv2d(X, num_outputs=256, kernel_size=9, stride=1, padding='VALID')
+
+	with tf.variable_scope('primary_capsule'): # Primary Capsules, [batch_size, 1152, 8, 1]
+	    primaryCaps = CapsConv(num_units=8, with_routing=False)
+	    caps1 = primaryCaps(conv1, num_outputs=32, kernel_size=9, stride=2)
+
+	with tf.variable_scope('digit_capsule'): # DigitCaps layer, [batch_size, 10, 16, 1]
+	    digitCaps = CapsConv(num_units=16, with_routing=True)
+	    caps2 = digitCaps(caps1, num_outputs=10)
+
+	with tf.variable_scope('masking'):
+	    v_length = tf.sqrt(tf.reduce_sum(tf.square(self.caps2), axis=2, keep_dims=True)) # calculate ||v_c||
+	    softmax_v = tf.nn.softmax(v_length, dim=1)
+
+	    argmax_ind = tf.argmax(softmax_v, axis=1, output_type=tf.int32)
+
+	    masked_v = []
+	    argmax_ind = tf.reshape(argmax_ind, shape=(batch_size, ))
+	    for batch in range(0, batch_size): # TODO: example code looks buggy here with "for batchsize in range(batchsize)"...
+	        v = caps2[batch][argmax_ind[batch], :]
+	        masked_v.append(tf.reshape(v, shape=(1, 1, 16, 1)))
+
+	    masked_v = tf.concat(masked_v, axis=0)
+
+	with tf.variable_scope('fully_connected'): # fully connected layers
+	    j_vec = tf.reshape(masked_v, shape=(batch_size, -1))
+	    fc1 = tf.contrib.layers.fully_connected(j_vec, num_outputs=512)
+	    fc2 = tf.contrib.layers.fully_connected(fc1, num_outputs=1024)
+	    pred = tf.contrib.layers.fully_connected(fc2, num_outputs=784, activation_fn=tf.sigmoid)
+
+
+
+
+
